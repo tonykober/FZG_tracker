ec1bf12ea1a35e5cc9a0df9118b95a17c478f31b
ACD_RD1謝岱錡 <koberhsieh@igs.com.tw>
2026-05-08 15:50:29 +0800

    refactor: 程式碼拆分(HTML/CSS/JS)、離線快取(SW)、錯誤處理重試按鈕

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'fzg-tracker-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
