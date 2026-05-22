1fa7d181faf217353ac4aa9bc1927f55938c036a
ACD_RD1謝岱錡 <koberhsieh@igs.com.tw>
2026-05-22 08:38:20 +0800

    feat: 自動建立新版本 t03

---
+++ t03/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  title: 'FZG 專案追蹤器',
+  sheetId: '1t3G0llImW01aT5k5KUXHZ6mZ8es_1DVvalvk74diBP8',
+  scriptUrl: 'https://script.google.com/macros/s/AKfycbynKuRWHpNOFH-D2roCpeWgtUufW0m9SOfq_3Mw0WxtqrFxIIayIHB31vXlCMxJcaQAUw/exec',
+  outsourceSheetId: '',
+  outsourceScriptUrl: '',
+  dailySheetId: '',
+  password: '0000'
+};
