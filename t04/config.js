a2895cba94d590b8da749788e5dd5885ed29fb1e
ACD_RD1謝岱錡 <koberhsieh@igs.com.tw>
2026-05-22 10:18:29 +0800

    feat: 自動建立新版本 t04（含外包）

---
+++ t04/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  title: 'FZG 專案追蹤器',
+  sheetId: '1t3G0llImW01aT5k5KUXHZ6mZ8es_1DVvalvk74diBP8',
+  scriptUrl: 'https://script.google.com/macros/s/AKfycbynKuRWHpNOFH-D2roCpeWgtUufW0m9SOfq_3Mw0WxtqrFxIIayIHB31vXlCMxJcaQAUw/exec',
+  outsourceSheetId: '17zIJez4UleSHb-yjaQXfEIGsbhgnbK6WPSe67GyZA5w',
+  outsourceScriptUrl: 'https://script.google.com/macros/s/AKfycbytzb7iQdvqRXCIKUt3tHYwXMvTnkmKrkNkAYWql4ttIPZArVKiqeAWiH_Pq3A1qRzY/exec',
+  dailySheetId: '',
+  password: '0000'
+};
